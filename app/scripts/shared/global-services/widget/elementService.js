a61a7fccb3b45897c73e9465f128c8747eb12272
katie <katie>
2016-08-10 18:14:32 +0800

    adjust login and sign & add switch effect

---
+++ app/scripts/shared/global-services/widget/elementService.js
@@ -0,0 +1,54 @@
+/**
+ * @ngdoc service
+ * @name touhou.ElementService
+ * @description
+ * 
+ * @example
+ * function exampleCtrl(..., ElementService) {
+ *     ...
+ * }
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('touhou')
+        .factory('ElementService', ElementService);
+
+    /* @ngInject */
+    function ElementService($rootScope, Restangular) {
+        // Initialize ElementService
+        var service = {
+            getByClass: getByClass
+        };
+        return service;
+
+        //
+        /**
+         * @description
+         * get element by class
+         * @example
+         * ElementService.getByClass('li', 'active', 'nav-pills')
+         */
+        function getByClass(dom, className, parentClass) {
+            var domArr = angular.element(document).find(dom);
+            var target = null;
+            angular.forEach(domArr, function(item, index) {
+                if(parentClass && parentClass !== '') {
+                    if(angular.element(item).hasClass(className) && angular.element(item).parent().hasClass(parentClass)){
+                        target = item;
+                        return;
+                    }
+                }else {
+                    if(angular.element(item).hasClass(className)){
+                        target = item;
+                        return;
+                    }
+                }
+            });
+            return target;
+        }
+    }
+
+})();
--- app/scripts/shared/templates/header/headerCtrl.js
+++ app/scripts/shared/templates/header/headerCtrl.js
@@ -14,7 +14,25 @@
         .controller('HeaderCtrl', HeaderCtrl);
 
     /* @ngInject */
-    function HeaderCtrl($rootScope, $state) {
+    function HeaderCtrl($document, ElementService) {
         var vm = this;
+
+        //main menu switch effect
+        var navPills = ElementService.getByClass('ul', 'nav-pills');
+        angular.element(navPills).find('li').on('click', function(e) {
+        	if(angular.element(e.target).parent().hasClass('active')) {
+        		return;
+        	}else{
+        		var liActive = ElementService.getByClass('li', 'active', 'nav-pills');
+        		angular.element(liActive).removeClass('active');
+        		angular.element(e.target).parent().addClass('active');
+        	}
+        });
+        var loginBtn = ElementService.getByClass('div', 'login');
+        var signBtn = ElementService.getByClass('div', 'sign');
+        angular.element(loginBtn).on('click', function(e) {
+        	var liActive = ElementService.getByClass('li', 'active', 'nav-pills');
+        	angular.element(liActive).removeClass('active');
+        });
     }
 })();
