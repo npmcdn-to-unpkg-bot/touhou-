27c230e8e3e226d68cd9bf4003cd8d6c17118aff
youranstray@163.com <youranstray@163.com>
2016-08-19 00:03:56 +0800

    adjust personal center page

--- app/scripts/routes/login/loginCtrl.js
+++ app/scripts/routes/login/loginCtrl.js
@@ -15,16 +15,17 @@
     function LoginCtrl($http, $state, $window, Restangular) {
         var vm = this;
         vm.login = login;
+        console.log(md5(vm.password));
 
+        // login
         function login() {
-        	console.log(vm.password);
         	 Restangular.setDefaultHeaders({
                 'Content-Type'  : 'application/json;charset=UTF-8'
-                //'Access-Control-Allow-Origin': '*',
             });
+
         	Restangular.all('api/user/signIn/' + vm.phone + '/' + vm.password).customGET().then(function(res) {
                 if(res.success) {
-                    
+                    console.log(res);
                 }
         	});
         }
