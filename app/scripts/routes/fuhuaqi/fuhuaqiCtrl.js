46a07b45a99c20e304623bca06ffa9e1c684fabd
youranstray <youranstray@163.com>
2016-08-18 23:29:21 +0800

    tuijian

---
+++ app/scripts/routes/fuhuaqi/fuhuaqiCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('touhou')
+        .controller('PuhuaqiCtrl', PuhuaqiCtrl);
+
+    /* @ngInject */
+    function PuhuaqiCtrl($http, $state, $window) {
+        var vm = this;
+    }
+})();
