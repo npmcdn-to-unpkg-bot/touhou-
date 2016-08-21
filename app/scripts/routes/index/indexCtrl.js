07ce7c4496ee51eb8e404e84a070cbd2a6c410a2
youranstray <youranstray@163.com>
2016-08-21 13:14:02 +0800

    index banner

--- app/scripts/routes/index/indexCtrl.js
+++ app/scripts/routes/index/indexCtrl.js
@@ -14,65 +14,131 @@
     /* @ngInject */
     function IndexCtrl($http, $state, $window, ElementService, MoveService) {
         var vm = this;
-        var n = 0;
-        var oDiv = ElementService.getByClass('div', 'index_banner');
-        var oUl = angular.element(oDiv).find('ul')[0];//angular.element(oDiv).find('ul')
-        var oWidth = ElementService.getStyle(oDiv,'width');
-        oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
-        var aLi = angular.element(oUl).find('li');
-        angular.element(oUl).css({width: parseFloat(oWidth) * aLi.length+'px'})//angular.element(oUl).css()
-       	for (var i = 0; i < aLi.length; i++) {
-            aLi[i].style.width = oWidth;
-        }
-        vm.rightClick = rightClick;
-        vm.leftClick = leftClick;
-        vm.mouser = mouser;
-        vm.mouseleave = mouseleave;
-        time()
-        var bl = true;
-        var timer;
-        function rightClick() {
-            if (bl) {
-            	n--;
-            	bl = !bl
-                 if (n == -1) {
-                        n = aLi.length/2-1;
-                        oUl.style.left = -(n+1) * parseFloat(oWidth) + 'px';
-                    }
-            	MoveService.move(oUl, {'left': -n * parseFloat(oWidth)},{fn:function(){
-                   
-                    bl = !bl;
-                }})
+        banner('div', 'ul', 'li', 'index_banner');
+       function banner(obj, obj1, obj2, className) {
+            var n = 0;
+            var oDiv = ElementService.getByClass(obj, className);
+            var oUl = angular.element(oDiv).find(obj1)[0];//angular.element(oDiv).find('ul')
+            var oWidth = ElementService.getStyle(oDiv,'width');
+            oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
+            var aLi = angular.element(oUl).find(obj2);
+            angular.element(oUl).css({width: parseFloat(oWidth) * aLi.length+'px'})//angular.element(oUl).css()
+            for (var i = 0; i < aLi.length; i++) {
+                aLi[i].style.width = oWidth;
             }
-        	//debugger
-        }
-        function leftClick(){
-            if (bl) {
-                bl=!bl;
-                tab();
-            }
-        }
-        function tab(){
-            n++;
-            MoveService.move(oUl, {'left': -n * parseFloat(oWidth)},{fn:function(){
-                   if (n == aLi.length/2) {
-                        n = 0;
-                        oUl.style.left = -n * parseFloat(oWidth) + 'px';
-                   }
-                    bl = true;
-                }})
-        }
-        function time(){
-            timer = setInterval(function(){
-                tab();
-            },2000)
-        }
-        function mouser() {
-            clearInterval(timer)
-        }
-        function mouseleave(){
+            vm.rightClick = rightClick;
+            vm.leftClick = leftClick;
+            vm.mouser = mouser;
+            vm.mouseleave = mouseleave;
             time()
-        }
+            var bl = true;
+            var timer;
+            function rightClick() {
+                if (bl) {
+                    n--;
+                    bl = !bl
+                     if (n == -1) {
+                            n = aLi.length/2-1;
+                            oUl.style.left = -(n+1) * parseFloat(oWidth) + 'px';
+                        }
+                    MoveService.move(oUl, {'left': -n * parseFloat(oWidth)},{fn:function(){
+                       
+                        bl = !bl;
+                    }})
+                }
+                //debugger
+            }
+            function leftClick(){
+                if (bl) {
+                    bl=!bl;
+                    tab();
+                }
+            }
+            function tab(){
+                n++;
+                MoveService.move(oUl, {'left': -n * parseFloat(oWidth)},{fn:function(){
+                       if (n == aLi.length/2) {
+                            n = 0;
+                            oUl.style.left = -n * parseFloat(oWidth) + 'px';
+                       }
+                        bl = true;
+                    }})
+            }
+            function time(){
+                timer = setInterval(function(){
+                    tab();
+                },2000)
+            }
+            function mouser() {
+                clearInterval(timer)
+            }
+            function mouseleave(){
+                time()
+            }
+       }
+       banner1('div', 'ul', 'li', 'in_list');
+       banner1('div', 'ul', 'li', 'in_xiangm');
+       function banner1(obj, obj1, obj2, className) {
+            var a = 0;
+            var oDiv = ElementService.getByClass(obj, className);
+            var oUl = angular.element(oDiv).find(obj1)[0];//angular.element(oDiv).find('ul')
+            oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
+            var aLi = angular.element(oUl).find(obj2);
+            var oWidth = ElementService.getStyle(aLi[0],'width');
+            angular.element(oUl).css({width: parseFloat(oWidth) * aLi.length+'px'})//angular.element(oUl).css()
+            for (var i = 0; i < aLi.length; i++) {
+                aLi[i].style.width = oWidth;
+            }
+            vm.rightClick1 = rightClick1;
+            vm.leftClick1 = leftClick1;
+            vm.mouser1 = mouser1;
+            vm.mouseleave1 = mouseleave1;
+            time1()
+            var bl = true;
+            var timer1;
+            function rightClick1() {
+                if (bl) {
+                    a--;
+                    bl = !bl
+                     if (a == -1) {
+                            a = aLi.length/2-1;
+                            oUl.style.left = -(a+1) * parseFloat(oWidth) + 'px';
+                        }
+                    MoveService.move(oUl, {'left': -a * parseFloat(oWidth)},{fn:function(){
+                       
+                        bl = !bl;
+                    }})
+                }
+                //debugger
+            }
+            function leftClick1(){
+                if (bl) {
+                    bl=!bl;
+                    tab();
+                }
+            }
+            function tab(){
+                a++;
+                MoveService.move(oUl, {'left': -a * parseFloat(oWidth)},{fn:function(){
+                       if (a == aLi.length/2) {
+                            a = 0;
+                            oUl.style.left = -a * parseFloat(oWidth) + 'px';
+                       }
+                        bl = true;
+                    }})
+            }
+            function time1(){
+                timer1 = setInterval(function(){
+                    tab();
+                },2000)
+            }
+            function mouser1() {
+                clearInterval(timer1)
+            }
+            function mouseleave1(){
+                time1()
+            }
+       }
         
     }
 })();
--- app/scripts/shared/global-services/widget/moveService.js
+++ app/scripts/shared/global-services/widget/moveService.js
@@ -20,7 +20,8 @@
     function MoveService($rootScope, Restangular, ElementService) {
         // Initialize MoveService
         var service = {
-            move: move
+            move: move,
+            banner1:banner1
         };
         return service;
 
@@ -77,6 +78,67 @@
 				}
 			},30);
 		}
+		function banner1(obj, obj1, obj2, className) {
+            var a = 0;
+            var oDiv = ElementService.getByClass(obj, className);
+            var oUl = angular.element(oDiv).find(obj1)[0];//angular.element(oDiv).find('ul')
+            oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
+            var aLi = angular.element(oUl).find(obj2);
+            var oWidth = ElementService.getStyle(aLi[0],'width');
+            angular.element(oUl).css({width: parseFloat(oWidth) * aLi.length+'px'})//angular.element(oUl).css()
+            for (var i = 0; i < aLi.length; i++) {
+                aLi[i].style.width = oWidth;
+            }
+            vm.rightClick1 = rightClick1;
+            vm.leftClick1 = leftClick1;
+            vm.mouser1 = mouser1;
+            vm.mouseleave1 = mouseleave1;
+            time1()
+            var bl = true;
+            var timer1;
+            function rightClick1() {
+                if (bl) {
+                    a--;
+                    bl = !bl
+                     if (a == -1) {
+                            a = aLi.length/2-1;
+                            oUl.style.left = -(a+1) * parseFloat(oWidth) + 'px';
+                        }
+                    MoveService.move(oUl, {'left': -a * parseFloat(oWidth)},{fn:function(){
+                       
+                        bl = !bl;
+                    }})
+                }
+                //debugger
+            }
+            function leftClick1(){
+                if (bl) {
+                    bl=!bl;
+                    tab();
+                }
+            }
+            function tab(){
+                a++;
+                MoveService.move(oUl, {'left': -a * parseFloat(oWidth)},{fn:function(){
+                       if (a == aLi.length/2) {
+                            a = 0;
+                            oUl.style.left = -a * parseFloat(oWidth) + 'px';
+                       }
+                        bl = true;
+                    }})
+            }
+            function time1(){
+                timer1 = setInterval(function(){
+                    tab();
+                },2000)
+            }
+            function mouser1() {
+                clearInterval(timer1)
+            }
+            function mouseleave1(){
+                time1()
+            }
+       }
     }
 
 })();
