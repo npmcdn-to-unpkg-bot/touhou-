13aec0181b5e74e21a91da9e2c836af1f7d1aa0f
youranstray <youranstray@163.com>
2016-08-30 23:05:03 +0800

    shoucangjia

--- app/scripts/routes/favorites/favoritesCtrl.js
+++ app/scripts/routes/favorites/favoritesCtrl.js
@@ -6,8 +6,79 @@
         .controller('FavoritesCtrl', FavoritesCtrl);
 
     /* @ngInject */
+   
     function FavoritesCtrl($http, $state, $window, Restangular) {
         var vm = this;
+        vm.favoriteList = [];
+        vm.json = {
+        	SEED:'种子轮',
+        	ANGEL:'天使轮',
+        	PRE_A:'Pre-A轮',
+        	A:'A',
+        	B:'B',
+        	C:'C'
+        };
+
+        var res ={
+		  "content": {
+		    "content": [
+		      {
+		        "address": "上海",
+		        "description": "金融",
+		        "financingPhase": "SEED",
+		        "foundDate": "2016/2/2",
+		        "founderId": "2112",
+		        "id": "2333",
+		        "industry": "服务",
+		        "integrity": "RED",
+		        "inviterId": "string",
+		        "isPrivate": true,
+		        "level": 0,
+		        "name": "丰厚资本",
+		        "occupied": true,
+		        "occupiedUser": "伍凯",
+		        "publicity": "公开",
+		        "region": "string",
+		        "status": "OBSERVATION"
+		      },
+		      {
+		        "address": "上海",
+		        "description": "金融",
+		        "financingPhase": "SEED",
+		        "foundDate": "2016/2/2",
+		        "founderId": "2112",
+		        "id": "2333",
+		        "industry": "服务",
+		        "integrity": "RED",
+		        "inviterId": "string",
+		        "isPrivate": true,
+		        "level": 0,
+		        "name": "丰厚资本",
+		        "occupied": true,
+		        "occupiedUser": "伍凯",
+		        "publicity": "公开",
+		        "region": "string",
+		        "status": "OBSERVATION"
+		      }
+		    ],
+		    "page": 0,
+		    "size": 0,
+		    "total": 0
+		  },
+		  "errMessage": "string",
+		  "success": true
+		}
+		if(res.success){
+			var oUl = document.getElementById('xm_list');
+			vm.favoriteList = res.content.content;
+			function showzhan() {
+				var oZan = document.getElementById('zhanweijiyou');
+			}
+			
+		}
+
+		vm.showZhan = showZhan;
+
         function getfavor(color, industry, page, query, round, size) {
         	var getlist = {};
         	if (color) {
@@ -28,14 +99,25 @@
         	if (size) {
         		getlist.size = size;
         	}
-        	
-        	Restangular.all('investor/project/invested/list').customPOST({
-				  "page": 1,
-				  "size": 1
-	        }).then(function(res) {debugger;
-	               console.log(res)
-	        	});
+        	Restangular.setDefaultHeaders({
+                'Content-Type'  : 'application/json;charset=UTF-8',
+                // 'Access-Control-Allow-Origin': '*'
+            });
+            Restangular.setDefaultHttpFields({withCredentials: true});
+        	Restangular.all('investor/project/invested/list').customPOST({page: 1, size: 1}).then(function(res) {
+        			
+	               console.log(res);
+	           });
         }
-        getfavor()  
+        getfavor();
+
+        function showZhan(index) {
+        	if(vm.favoriteList[index].displayOccupy) {
+        		vm.favoriteList[index].displayOccupy = false;
+        	}else {
+        		vm.favoriteList[index].displayOccupy = true;
+        	}
+        	
+        }
     }
 })();
